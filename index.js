e6eb8ae9e181521f45e620df534c8f896f777c5f
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-21 05:27:10 -0400

    created a function that will display students name

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const students = [{name:"Ziphozonke", mark:"25"}, {name:"Siphelele", mark:"34"},
  {name:"Thembelani", mark:"28"},{name:"Kwanele", mark:"40"}, {name:"Yandisa", mark:"45"}]
 
-console.log()
+function displayFunction(name){
+var name = new Array()
+
+
+}
+console.log(name)
