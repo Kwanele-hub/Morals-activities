5be2f311024fcfa0927887aa6d9acc89195c28e1
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-21 05:05:54 -0400

    added list of array of students

---
+++ index.js
@@ -0,0 +1,4 @@
+const students = [{name:"Ziphozonke", mark:"25"}, {name:"Siphelele", mark:"34"},
+ {name:"Thembelani", mark:"28"},{name:"Kwanele", mark:"40"}, {name:"Yandisa", mark:"45"}]
+
+console.log()
