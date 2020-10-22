4577e7297ae7b7a1423369ed46465b4d4032ed27
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-22 09:50:32 -0400

    on line 11 i created a function for average

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const students = [{name:"Ziphozonke", mark:"25"}, {name:"Siphelele", mark:"34"},
 function displayStudents(students){
 let names = students.map((student)=>student.name)
 return names
-// const marks = [];
-
-// function getAvg(grades) {
-//   const total = grades.reduce((acc, c) => acc + c, 0);
-//   return total / grades.length;
-// }
-
-// const average = getAvg(grades)};
-// console.log(grades);
-// 
 }
-console.log(displayStudents(students))
+// console.log(displayStudents(students))
+const grades = [25, 34, 28, 40, 45];
+
+function getAvg(grades) {
+  const total = grades.reduce((acc, c) => acc + c, 0);
+  return total / grades.length;
+}
+
+const average = getAvg(grades);
+console.log(average);
