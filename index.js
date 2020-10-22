6ae706dd938dc9c13d7cf57f270ebe79cdb78d65
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-22 09:13:08 -0400

    added the let statement that will return the names of students

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const students = [{name:"Ziphozonke", mark:"25"}, {name:"Siphelele", mark:"34"},
  {name:"Thembelani", mark:"28"},{name:"Kwanele", mark:"40"}, {name:"Yandisa", mark:"45"}]
 
-function displayFunction(name){
-var name = new Array()
+function displayStudents(students){
+let names = students.map((student)=>student.name)
+return names
+// const marks = [];
 
+// function getAvg(grades) {
+//   const total = grades.reduce((acc, c) => acc + c, 0);
+//   return total / grades.length;
+// }
 
+// const average = getAvg(grades)};
+// console.log(grades);
+// 
 }
-console.log(name)
+console.log(displayStudents(students))
