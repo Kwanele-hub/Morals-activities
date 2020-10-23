3ce926bcc958dde21c232fe2e3255e264450ef74
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-23 03:01:31 -0400

    on line 19 i added a function of topachieve that should return an object of top students

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function getAvg(grades) {
 
 const average = getAvg(grades);
 console.log(average);
+
+function topAchieve(students){
+  let names = students.map((student) =>student.name)
+  return students = 2
+}
+console.log(topAchieve(students))
