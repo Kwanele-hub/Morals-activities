a6370312bf3a5d480c212f19dafa0537e33a7439
nolwazi749 <nolwazizwane7@gmail.com>
2020-11-17 07:46:51 -0500

    added topAchievers

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ function displayStudents(students){
 let names = students.map((student)=>student.name)
 return names
 }
-// console.log(displayStudents(students))
+console.log(displayStudents(students))
+// 
 const grades = [25, 34, 28, 40, 45];
 
 function getAvg(grades) {
   const total = grades.reduce((acc, c) => acc + c, 0);
   return total / grades.length;
-}
-
+};
+// 
 const average = getAvg(grades);
 console.log(average);
 
 function topAchieve(students){
   let names = students.map((student) =>student.name)
-  return students = 2
+  return 
 }
 console.log(topAchieve(students))
