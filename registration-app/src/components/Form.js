a9b8835f611f9fcbc24586694da76948a83a7c80
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-02-01 08:28:29 +0200

    created a folder of components, with its file for form.js

---
+++ registration-app/src/components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Form extends React.Component(){
+    
+    render(){
+        return(
+            <div className='Form'>
+                <h1>registration form</h1>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default Form;
