114571ab2f2f4f74d49c58cd05a240f9b58ee65d
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-02-02 13:15:25 +0200

    created a form on the Form file, also added the state

--- registration-app/src/components/Form.js
+++ registration-app/src/components/Form.js
@@ -1,18 +1,47 @@
 import React from 'react';
 
-class Form extends React.Component(){
-    
+class Form extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state ={
+            name:'',
+            surname:'',
+            Id: '',
+            age:'',
+            allergies: '',
+            address: '',
+        }
+        // this.handleSubmit=this.handleSubmit()
+
+    }
+
+
+
     render(){
         return(
-            <div className='Form'>
-                <h1>registration form</h1>
+            <div className='form'>
+                <fieldset>
+                <form>
+                    <input type='text' placeholder='name' value={this.name}></input>
+                    
+                    <input type='text' placeholder='surname' value={this.surname}></input>
+                    
+                    <input type='text' placeholder='Id' value={this.Id}></input>
+                    
+                    <input type='text' placeholder='Age' value={this.age}></input>
+                    
+                    <input type='text' placeholder='Allergies' value={this.allergies}></input>
+
+                    <input type='text' placeholder='Address' value={this.address}></input>
+
+                    <input type='submit'></input>
+                </form>
+
+                </fieldset>
+                
             </div>
         )
     }
 }
-
-
-
-
-
-export default Form;
+export default Form
