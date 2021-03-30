c8a29ad3f8e06085ef35a418d487c2c64a93095c
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-03-30 13:00:23 +0200

    added new file

---
+++ list-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Inputs from './components/Inputs';
+
+function App() {
+  return (
+    <div className="App">
+      <Inputs/>
+    </div>
+  );
+}
+
+export default App;
